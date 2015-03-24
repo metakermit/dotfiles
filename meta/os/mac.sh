168042720933790c036049a359c17a0c0944911c
Dražen Lučanin <kermit666@gmail.com>
2015-03-24 22:06:54 +0100

    the basics only for now

--- meta/os/mac.sh
+++ meta/os/mac.sh
@@ -12,45 +12,49 @@ sudo curl $key_repo/Croatian-awesome.icns \
 
 # Xcode command line tools
 #-------------------------
-xcode-select --install
+# let's see if it's needed
+# xcode-select --install
 
 # Homebrew
 #---------
-ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
 
+# first things first
+brew install git
+
 # brew install bash-completion
-brew install ssh-copy-id wget
+#brew install ssh-copy-id wget
 
 # Python
 #--------
 # SSL better from brew
 # http://hackercodex.com/guide/python-development-environment-on-mac-osx/
-echo "Leg-stretching while Python installs..."
-brew install python --with-brewed-openssl
+#echo "Leg-stretching while Python installs..."
+#brew install python --with-brewed-openssl
 
 # Py3k
-brew install python3 --with-brewed-openssl
+#brew install python3 --with-brewed-openssl
 
 # now on with the packages...
 # http://www.lowindata.com/2013/installing-scientific-python-on-mac-os-x/
-pip install virtualenv
-pip install virtualenvwrapper
+#pip install virtualenv
+#pip install virtualenvwrapper
 
 # the rest I install in kermit-install-packages-local
 #pip install numpy
-echo "Now go for a coffee, gfortran takes a long time..."
-brew install gfortran
-#pip install scipy
-brew install freetype
-#pip install matplotlib
-#pip install ipython
+#echo "Now go for a coffee, gfortran takes a long time..."
+#brew install gfortran
+# #pip install scipy
+#brew install freetype
+# #pip install matplotlib
+# #pip install ipython
 
 # R
 #---
-brew tap homebrew/science
-echo "Install XQuartz from https://xquartz.macosforge.org/landing/"
-brew install R
+#brew tap homebrew/science
+#echo "Install XQuartz from https://xquartz.macosforge.org/landing/"
+#brew install R
 
 # Cask
 brew tap cask
